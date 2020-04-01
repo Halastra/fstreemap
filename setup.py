4246467a9577cc5317261e6d052ed5dc887458b3
Or Barzilay <turchy@gmail.com>
2020-04-01 22:29:26 +0300

    Updated package attributes in setup.py: Updated attributes: name, author, author_email, description

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",  # Replace with your own username
+    name="etree-Halastra",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Or Barzilay",
+    author_email="turchy@gmail.com",
+    description="Interactive analysis treemaps for filesystems",
     long_description=long_description,
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
